cbebb9a749aa8e72a5b7d7d8758e7446b4a512cd
Dmitry Doni <dmitry.doni@gmail.com>
2020-01-19 13:52:30 +0100

    Task 5

---
+++ task_5/task_5.py
@@ -0,0 +1,9 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+letter_number = int(input("Please enter letter number (1...26): "))
+
+if 0 < letter_number < 27:
+    alphabet_index = letter_number - 1
+    print("Letter: ", alphabet[alphabet_index])
+else:
+    print("Invalid number. Please note that English alphabet consists of 26 letters.")
